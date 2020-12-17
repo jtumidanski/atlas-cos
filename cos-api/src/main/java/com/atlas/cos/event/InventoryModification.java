4f14a0b83d17851038c8d6f9fb91ecfe71e32f6c
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2020-12-17 15:55:44 -0500

    Add item id to event.

--- cos-api/src/main/java/com/atlas/cos/event/InventoryModification.java
+++ cos-api/src/main/java/com/atlas/cos/event/InventoryModification.java
@@ -1,7 +1,7 @@
 package com.atlas.cos.event;
 
-public record InventoryModification(int mode, int inventoryType, int quantity, int position, int oldPosition) {
-   public InventoryModification(int mode, int inventoryType, int quantity, int position) {
-      this(mode, inventoryType, quantity, position, 0);
+public record InventoryModification(int mode, int itemId, int inventoryType, int quantity, int position, int oldPosition) {
+   public InventoryModification(int mode, int itemId, int inventoryType, int quantity, int position) {
+      this(mode, itemId, inventoryType, quantity, position, 0);
    }
 }
